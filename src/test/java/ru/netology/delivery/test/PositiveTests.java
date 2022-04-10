fa40deccef1d415096f5a970ac632e846db600a6
Alexander Maletin <man.alex212@gmail.com>
2022-04-10 17:15:41 +0500

    Added Allure library, test-reports + Gradle test-reports

--- src/test/java/ru/netology/delivery/test/PositiveTests.java
+++ src/test/java/ru/netology/delivery/test/PositiveTests.java
@@ -1,6 +1,10 @@
 package ru.netology.delivery.test;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -21,6 +25,16 @@ public class PositiveTests {
     @BeforeEach
     public void setupTest() {
         open("http://localhost:9999");
+    }
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
     }
 
     String closestDate = generateDate(3);
